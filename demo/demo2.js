3e41f34462a5e304964cd2467b24849ca7faf8e2
Daniel Kawalsky <dkawalsk@usc.edu>
2020-12-30 00:07:25 -0600

    removed download code from demo2.js

--- demo/demo2.js
+++ demo/demo2.js
@@ -22,52 +22,12 @@
 		alert(message);
 	}
 
-	function createTempFile(callback) {
-		var tmpFilename = "tmp.dat";
-		requestFileSystem(TEMPORARY, 4 * 1024 * 1024 * 1024, function(filesystem) {
-			function create() {
-				filesystem.root.getFile(tmpFilename, {
-					create : true
-				}, function(zipFile) {
-					callback(zipFile);
-				});
-			}
-
-			filesystem.root.getFile(tmpFilename, null, function(entry) {
-				entry.remove(create, create);
-			}, create);
-		});
-	}
-
 	var model = (function() {
-		var URL = obj.webkitURL || obj.mozURL || obj.URL;
-
 		return {
 			getEntries : function(file, onend) {
 				zip.createReader(new zip.BlobReader(file), function(zipReader) {
 					zipReader.getEntries(onend);
 				}, onerror);
-			},
-			getEntryFile : function(entry, creationMethod, onend, onprogress) {
-				var writer, zipFileEntry;
-
-				function getData() {
-					entry.getData(writer, function(blob) {
-						var blobURL = creationMethod == "Blob" ? URL.createObjectURL(blob) : zipFileEntry.toURL();
-						onend(blobURL);
-					}, onprogress);
-				}
-
-				if (creationMethod == "Blob") {
-					writer = new zip.BlobWriter();
-					getData();
-				} else {
-					createTempFile(function(fileEntry) {
-						zipFileEntry = fileEntry;
-						writer = new zip.FileWriter(zipFileEntry);
-						getData();
-					});
-				}
 			}
 		};
 	})();
@@ -128,7 +88,6 @@
 
 	function makeDOMInteractive() {
 		var fileInput = document.getElementById("file-input");
-		var unzipProgress = document.createElement("progress");
 		var fileList = document.getElementById("file-list");
 		var fileProgressMeterList = document.getElementById("file-progress-meter-list");
 		var fileProgressMeter = document.getElementById("file-progress-meter");
@@ -140,24 +99,6 @@
 			maxZoom: 18,
 			minZoom: 1
 		} ).addTo( map );
-
-		function download(entry, li, a) {
-			model.getEntryFile(entry, creationMethodInput.value, function(blobURL) {
-				var clickEvent = document.createEvent("MouseEvent");
-				if (unzipProgress.parentNode)
-					unzipProgress.parentNode.removeChild(unzipProgress);
-				unzipProgress.value = 0;
-				unzipProgress.max = 0;
-				clickEvent.initMouseEvent("click", true, true, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
-				a.href = blobURL;
-				a.download = entry.filename;
-				a.dispatchEvent(clickEvent);
-			}, function(current, total) {
-				unzipProgress.value = current;
-				unzipProgress.max = total;
-				li.appendChild(unzipProgress);
-			});
-		}
 
 		function processSegment (segment, index, year, month) {
 			if (segment?.activitySegment) {
@@ -322,21 +263,6 @@
 				};
 				fileProgressMeter.textContent = getTotalReadFileProgress();
 			});
-			
-
-			var li = document.createElement("li");
-			var a = document.createElement("a");
-			a.textContent = entry.filename;
-			a.href = "#";
-			a.addEventListener("click", function(event) {
-				if (!a.download) {
-					download(entry, li, a);
-					event.preventDefault();
-					return false;
-				}
-			}, false);
-			li.appendChild(a);
-			fileList.appendChild(li);
 		}
 
 		function onUploadFile() {
